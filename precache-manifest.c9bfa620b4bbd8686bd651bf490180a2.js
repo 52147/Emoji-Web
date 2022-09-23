25124c61216e20b7c5f453ae3203ecdf79596d10
Your Name <debra4117@gmail.com>
2022-09-23 00:27:59 -0500

    Updates

---
+++ precache-manifest.c9bfa620b4bbd8686bd651bf490180a2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ba04b4a1a7b748116a049bcbeaaced",
+    "url": "/Emoji-Web/index.html"
+  },
+  {
+    "revision": "827efb21112e19d640a8",
+    "url": "/Emoji-Web/static/js/2.056525c0.chunk.js"
+  },
+  {
+    "revision": "8dd6a1158403d70e8e82",
+    "url": "/Emoji-Web/static/js/main.ba1ed8b1.chunk.js"
+  },
+  {
+    "revision": "d3e47340a2f1c709afe8",
+    "url": "/Emoji-Web/static/js/runtime-main.7fd115e6.js"
+  }
+]);
